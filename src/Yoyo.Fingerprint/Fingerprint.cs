b76056bf697a80e28e13022053580eedc7950c85
Mike Masey <mike.masey@yoyodesign.com>
2019-05-16 10:43:32 +0100

    fix: add try catch for if file does not exist

--- src/Yoyo.Fingerprint/Fingerprint.cs
+++ src/Yoyo.Fingerprint/Fingerprint.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -7,74 +8,83 @@ using System.Web.Hosting;
 
 namespace Yoyo.Fingerprint
 {
-	public class Fingerprint
-	{
-		/// <summary>
-		/// Adds a cache busting url for the file requested.
-		///
-		/// NOTE - In order for this to work, the following rewrite rule must exist in the web.config.
-		///
-		/// 	
-		/// 	<system.webServer>
-		/// 	  <rewrite>
-		/// 		<rules>
-		/// 		  <rule name="Yoyo.Fingerprint" >
-		/// 			<match url="([\S]+)(/v-[a-z0-9]+/)([\S]+)" />
-		/// 			<action type="Rewrite" url="{R:1}/{R:3}" />
-		/// 		  </rule>
-		/// 		</rules>
-		/// 	  </rewrite>
-		/// 	</system.webServer>
-		/// 												  
-		///
-		/// </summary>
-		/// <param name="rootRelativePath"></param>
-		/// <returns></returns>
-		public static string Tag(string rootRelativePath)
-		{
-			// Get absolute file path
-			string absolute = HostingEnvironment.MapPath("~" + rootRelativePath);
+    public class Fingerprint
+    {
+        /// <summary>
+        /// Adds a cache busting url for the file requested.
+        ///
+        /// NOTE - In order for this to work, the following rewrite rule must exist in the web.config.
+        ///
+        /// 	
+        /// 	<system.webServer>
+        /// 	  <rewrite>
+        /// 		<rules>
+        /// 		  <rule name="Yoyo.Fingerprint" >
+        /// 			<match url="([\S]+)(/v-[a-z0-9]+/)([\S]+)" />
+        /// 			<action type="Rewrite" url="{R:1}/{R:3}" />
+        /// 		  </rule>
+        /// 		</rules>
+        /// 	  </rewrite>
+        /// 	</system.webServer>
+        /// 												  
+        ///
+        /// </summary>
+        /// <param name="rootRelativePath"></param>
+        /// <returns></returns>
+        public static string Tag(string rootRelativePath)
+        {
+            // Get absolute file path
+            string absolute = HostingEnvironment.MapPath("~" + rootRelativePath);
 
-			if (string.IsNullOrWhiteSpace(absolute)) { return null; }
+            if (string.IsNullOrWhiteSpace(absolute)) { return null; }
 
-			// Check cache for path
-			if (HttpRuntime.Cache[rootRelativePath] == null)
-			{
-				// Open file
-				using (FileStream fs = new FileStream(absolute, FileMode.Open, FileAccess.Read, FileShare.Read))
-				{
-					// Create a hash out of the file strem
-					string hash = HashFile(fs);
-					int index = rootRelativePath.LastIndexOf('/');
+            // Check cache for path
+            if (HttpRuntime.Cache[rootRelativePath] == null)
+            {
+                try
+                {
 
-					// Create new url using hash
-					string result = rootRelativePath.Insert(index, "/v-" + hash);
-					HttpRuntime.Cache.Insert(rootRelativePath, result, new CacheDependency(absolute));
-				}
-			}
+                    // Open file
+                    using (FileStream fs = new FileStream(absolute, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        // Create a hash out of the file strem
+                        string hash = HashFile(fs);
+                        int index = rootRelativePath.LastIndexOf('/');
 
-			return HttpRuntime.Cache[rootRelativePath] as string;
-		}
+                        // Create new url using hash
+                        string result = rootRelativePath.Insert(index, "/v-" + hash);
+                        HttpRuntime.Cache.Insert(rootRelativePath, result, new CacheDependency(absolute));
+                    }
+                }
+                catch (Exception)
+                {
+                    // Is the file stream fails, return the initial passed in value
+                    return rootRelativePath;
+                }
+            }
 
-		private static string HashFile(FileStream stream)
-		{
-			StringBuilder sb = new StringBuilder();
+            return HttpRuntime.Cache[rootRelativePath] as string;
+        }
 
-			if (stream != null)
-			{
-				// Ensure stream is at the beginning
-				stream.Seek(0, SeekOrigin.Begin);
+        private static string HashFile(FileStream stream)
+        {
+            StringBuilder sb = new StringBuilder();
 
-				// Create the hash
-				MD5 md5 = MD5.Create();
-				byte[] hash = md5.ComputeHash(stream);
-				foreach (byte b in hash)
-					sb.Append(b.ToString("x2"));
+            if (stream != null)
+            {
+                // Ensure stream is at the beginning
+                stream.Seek(0, SeekOrigin.Begin);
 
-				stream.Seek(0, SeekOrigin.Begin);
-			}
+                // Create the hash
+                MD5 md5 = MD5.Create();
+                byte[] hash = md5.ComputeHash(stream);
+                foreach (byte b in hash)
+                    sb.Append(b.ToString("x2"));
 
-			return sb.ToString();
-		}
-	}
+                stream.Seek(0, SeekOrigin.Begin);
+            }
+
+            return sb.ToString();
+        }
+    }
 }
